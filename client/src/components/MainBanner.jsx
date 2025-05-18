2d99c229713b8f45805cd3cd1d27a96edf3e03b1
MarcAT92 <94722932+MarcAT92@users.noreply.github.com>
2025-05-18 14:12:34 -0400

    style(MainBanner): update heading text style with gradient background
    The change applies a gradient background to the heading text in the MainBanner component to enhance visual appeal and align with the updated design requirements.

--- client/src/components/MainBanner.jsx
+++ client/src/components/MainBanner.jsx
@@ -9,7 +9,7 @@ const MainBanner = () => {
             <img src={assets.main_banner_bg_sm} alt="banner" className="w-full md:hidden" />
 
             <div className='absolute inset-0 flex flex-col items-center md:items-start justify-end md:justify-center pb-20 md:pb-0 px-4 md:pl-18 lg:pl-24'>
-                <h1 className='text-3xl md:text-4xl lg:text-5xl font-bold text-center bg-primary-dull text-black md:text-left max-w-72 md:max-w-80 lg:max-w-105 leading-tight lg:leading-15'>Freshness You Can Trust, Savings You will Love!</h1>
+                <h1 className='text-3xl md:text-4xl lg:text-5xl font-bold text-center md:text-left max-w-72 md:max-w-80 lg:max-w-105 leading-tight lg:leading-15 bg-gradient-to-r from-green-400 to-green-700 text-transparent bg-clip-text'>Freshness You Can Trust, Savings You will Love!</h1>
 
                 <div className='flex items-center mt-6 font-medium'>
                     <Link to={"/products"} className='group flex items-center gap-2 px-7 md:px-9 py-3 bg-primary hover:bg-primary-dull transition rounded text-white cursor-pointer'>
