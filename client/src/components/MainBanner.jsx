94a85d349a4541360472f60114a689e18b9dd361
MarcAT92 <94722932+MarcAT92@users.noreply.github.com>
2025-05-20 11:08:11 -0400

    Update Mainbanner

--- client/src/components/MainBanner.jsx
+++ client/src/components/MainBanner.jsx
@@ -8,10 +8,10 @@ const MainBanner = () => {
             <img src={assets.main_banner_bg} alt="banner" className="w-full hidden md:block" />
             <img src={assets.main_banner_bg_sm} alt="banner" className="w-full md:hidden" />
 
-            <div className='absolute inset-0 flex flex-col items-center md:items-start justify-center md:justify-center pt-20 md:pb-20 px-4 md:px-8 lg:px-12 xl:px-24' >
-                <h1 className='text-3xl md:text-4xl lg:text-5xl font-bold text-center md:text-left max-w-xs md:max-w-sm lg:max-w-md leading-tight lg:leading-15 bg-gradient-to-r from-gray-900 to-gray-900 text-transparent bg-clip-text'>Freshness You Can Trust, Savings You will Love!</h1>
+            <div className='absolute inset-0 flex flex-col items-center md:items-start justify-center md:justify-center pt-10 pb-10 px-4 md:px-8 lg:px-12 xl:px-24' >
+                <h1 className='text-2xl md:text-4xl lg:text-5xl font-bold text-center md:text-left max-w-xs leading-tight lg:leading-15 bg-gradient-to-r from-green-600 to-green-900 text-transparent bg-clip-text'>Freshness<br className='md:hidden'/> You Can Trust,<br className='md:hidden'/>  Savings!<br className='md:hidden'/> You will Love!</h1>
 
-                <div className='flex items-center mt-6 font-medium'>
+                <div className='flex items-center mt-4 font-medium'>
                     <Link to={"/products"} onClick={() => window.scrollTo(0, 0)} className='group flex items-center gap-2 px-7 md:px-9 py-3 bg-primary hover:bg-primary-dull transition rounded text-white cursor-pointer'>
                         Shop Now
                         <img className='md:hidden transition group-focus:translate-x-1' src={assets.white_arrow_icon} alt='arrow' />
